e956c9735a13b6f663d517906d47edf087ce4454
Guilherme <shwndofus@gmail.com>
2025-06-09 18:10:15 -0300

    aula 7

---
+++ aula7.py
@@ -0,0 +1,11 @@
+nome_completo = 'Guilherme'
+
+
+# soma_dois_mais_dois = 2 + 2
+# print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Gui'
+idade = 24
+maior_de_idade = idade >= 18
+
+print('Nome: ', nome, 'a Idade é:', idade, ',', 'ou seja, ele é maior de idade? ', maior_de_idade)
