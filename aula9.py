3926062304fb1c65b177d7b5c9b4175e0c307e36
Guilherme <shwndofus@gmail.com>
2025-06-09 20:01:26 -0300

    aula 9

---
+++ aula9.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 2
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2
+print('Divisão', divisao)
+
+divisao_full = 10 // 10
+print('Divisão Inteira', divisao_full)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+resto = 25 % 2
+print('Resto da divisão', resto)
