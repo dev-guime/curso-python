bb74a7caeb8735b9cb380ebbd6b57c7e0b1f0ea7
Guilherme <shwndofus@gmail.com>
2025-06-09 19:52:51 -0300

    aula 8

---
+++ aula8.py
@@ -0,0 +1,15 @@
+nome = 'Gui'
+sobrenome = 'Mello'
+idade = 24
+ano_nascimento = 2025 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.75
+
+
+print ('Nome:', nome)
+print ('Sobrenome:', sobrenome)
+print ('Idade:', idade)
+print ('Nasceu em:', ano_nascimento)
+print ('É maior de idade?', maior_de_idade)
+print ('Altura em metros:', altura_metros)
+
